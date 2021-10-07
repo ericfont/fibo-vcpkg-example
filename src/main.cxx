5ab9245a3573bfb1b3f759b80dc577a510d24581
Eric Fontaine <ericfontainejazz@gmail.com>
2021-10-07 00:55:43 -0400

    int replaced with uintmax, and limits set

--- src/main.cxx
+++ src/main.cxx
@@ -1,15 +1,16 @@
 #include <cxxopts.hpp>
 #include <fmt/format.h>
 #include <range/v3/view.hpp>
+#include <cstdint>
 
 namespace view = ranges::views;
 
-int fib(int x) {
-  int a = 0, b = 1;
+uintmax_t fib(uintmax_t x) {
+  uintmax_t a = 0, b = 1;
 
   for (int it : view::repeat(0) | view::take(x)) {
     (void)it;
-    int tmp = a;
+    uintmax_t tmp = a;
     a += b;
     b = tmp;
   }
@@ -20,12 +21,17 @@ int fib(int x) {
 int main(int argc, char** argv) {
   cxxopts::Options options("fibo", "Print the fibonacci sequence up to a value 'n'");
     options.add_options()
-      ("n,value", "The value to print to", cxxopts::value<int>()->default_value("100"));
+      ("n,value", "The value to print to", cxxopts::value<uintmax_t>()->default_value("1000"));
 
+  uintmax_t max = UINTMAX_MAX / 2;
   auto result = options.parse(argc, argv);
-  auto n = result["value"].as<int>();
+  auto n = result["value"].as<uintmax_t>();
 
   for (int x : view::iota(1) | view::take(n)) {
+    if (fib(x) > max) {
+      fmt::print("exceeded maximum for uintmax_t / 2 = {}.\n", max);
+      break;
+    }
     fmt::print("fib({}) = {}\n", x, fib(x));
   }
 }
